c843bafac96f4db9acfc8a6eb68feb085da4b920
ZZF <347531289@qq.com>
2019-01-03 21:18:17 +0800

    添加方法

--- git_test/src/cn/itcast/get/TestDemo1.java
+++ git_test/src/cn/itcast/get/TestDemo1.java
@@ -4,7 +4,15 @@ public class TestDemo1 {
 
     public static void main(String[] args) {
         System.out.println("hello git");
+
+        method();
     }
+
+    private static void method() {
+        int i = 2;
+        System.out.println(i);
+    }
+
 
 }
 
